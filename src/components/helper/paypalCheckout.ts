7948b1efd226dd3fbb3d6b010a21410b39fd01f6
khantkyaw6 <khantkaneki@gmail.com>
2025-09-27 15:43:24 +0700

    update for vercel update

--- src/components/helper/paypalCheckout.ts
+++ src/components/helper/paypalCheckout.ts
@@ -3,7 +3,10 @@ import axios from 'axios';
 export async function handleCheckout() {
 	try {
 		// Optional: you can return a loading state from the caller if needed
-		const { data } = await axios.post('https://paypal-server-yujz.onrender.com/create-order');
+		const { data } = await axios.post(
+			'https://paypal-server-yujz.onrender.com/create-order'
+			// 'http://localhost:3001/create-order'
+		);
 		const orderId = data.id;
 
 		// Redirect to PayPal approval
