eecac4790ea89e97693dadbb690893ec19014319
khantkyaw6 <khantkaneki@gmail.com>
2025-10-11 15:00:38 +0700

    update

--- src/components/helper/stripeCheckout.ts
+++ src/components/helper/stripeCheckout.ts
@@ -3,13 +3,13 @@ import axios from 'axios';
 export async function handleStripeCheckout() {
 	try {
 		console.log('In Stripe checkout');
-		// const { data } = await axios.post(
-		// 	'https://paypal-server-yujz.onrender.com/create-stripe-session'
-		// );
-
 		const { data } = await axios.post(
-			'http://localhost:3001/create-stripe-session'
+			'https://paypal-server-yujz.onrender.com/create-stripe-session'
 		);
+
+		// const { data } = await axios.post(
+		// 	'http://localhost:3001/create-stripe-session'
+		// );
 
 		// Redirect to Stripe’s checkout page
 		window.location.href = data.url;
